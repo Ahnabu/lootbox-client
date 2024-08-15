8a32aab128a4470c3e09a0b9fef77b52b335d64d
Ahnabu <horairaabu013025@gmail.com>
2024-08-15 20:00:36 +0600

    brand and category filtering added

--- src/Components/Home/Products/Products.jsx
+++ src/Components/Home/Products/Products.jsx
@@ -31,6 +31,8 @@ const Products = () => {
         
     }, [])
     const [filter, setFilter] = useState('')
+    const [brand, setBrand] = useState('')
+    const [category, setCategory] = useState('')
     const [sort, setSort] = useState('')
     const [sortOrder, setSortOrder] = useState('');
     const [minPrice, setMinPrice] = useState('');
@@ -41,7 +43,9 @@ const Products = () => {
             const res = await axios.get(`http://localhost:5000/data?filter=${filter}&sort=${sort}&order=${sortOrder}`, {
                 params: {
                     minPrice: minPrice || undefined,
-                    maxPrice: maxPrice || undefined
+                    maxPrice: maxPrice || undefined,
+                    brand: brand,
+                    category: category
                 }
             })
                
@@ -59,6 +63,18 @@ const Products = () => {
         setFilter(filter)
         refetch()
     }
+    const handleBrand = (e) => {
+        e.preventDefault();
+        const Brand = e.target.search.value;
+        setBrand(Brand)
+        refetch()
+    }
+    const handleCategory = (e) => {
+        e.preventDefault();
+        const Category = e.target.search.value;
+        setCategory(Category)
+        refetch()
+    }
     const handleMinMax = () => {
         refetch()
     }
@@ -69,17 +85,51 @@ const Products = () => {
     // }, [])
     // console.log(data);
     return (
-        <div className="text-center">
+        <div className="text-center ">
             <h2 id="title" className="text-4xl mx-auto opacity-0 translate-y-2 transition-all text-black pt-20 md:pt-24">LootBox</h2>
             <p id="text" className="text-xl mx-auto opacity-0 translate-y-2 transition-all text-black pt-3 "> Discover a curated collection of top-quality products, all in one place. LootBox brings you the best deals, trusted brands, and a seamless shopping experience tailored just for you.</p>
 
-            <div className="my-4 md:flex justify-center gap-8 ">
+            <div className="my-4 md:flex justify-center items-center gap-8 ">
                
                 <div className="text-center md:mr-20  ">
                     <form className="w-32 space-y-1 dark:text-gray-800 mx-auto" onSubmit={handleFilter}>
                         <label htmlFor="Search" className="hidden">Search</label>
                         <div className="relative mx-auto">
                             <input type="search" name="search" placeholder="Search product name..." className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none text-black bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-violet-600" />
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+
+                                <button type="submit" title="search" className="p-1  focus:outline-none focus:ring">
+                                    <svg fill="currentColor" viewBox="0 0 512 512" className="w-4 h-4 dark:text-gray-800 text-primary">
+                                        <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
+                                    </svg>
+                                </button>
+                            </span>
+
+                        </div>
+                    </form>
+                </div>
+                <div className="text-center md:mr-20  ">
+                    <form className="w-32 space-y-1 dark:text-gray-800 mx-auto" onSubmit={handleBrand}>
+                        <label htmlFor="Search" className="hidden">Search</label>
+                        <div className="relative mx-auto">
+                            <input type="search" name="search" placeholder="Search brand..." className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none text-black bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-violet-600" />
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+
+                                <button type="submit" title="search" className="p-1  focus:outline-none focus:ring">
+                                    <svg fill="currentColor" viewBox="0 0 512 512" className="w-4 h-4 dark:text-gray-800 text-primary">
+                                        <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
+                                    </svg>
+                                </button>
+                            </span>
+
+                        </div>
+                    </form>
+                </div>
+                <div className="text-center md:mr-20  ">
+                    <form className="w-32 space-y-1 dark:text-gray-800 mx-auto" onSubmit={handleCategory}>
+                        <label htmlFor="Search" className="hidden">Search</label>
+                        <div className="relative mx-auto">
+                            <input type="search" name="search" placeholder="Search category..." className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none text-black bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-violet-600" />
                             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
 
                                 <button type="submit" title="search" className="p-1  focus:outline-none focus:ring">
@@ -102,7 +152,7 @@ const Products = () => {
                                 onClick={() => { setSort('price'), refetch() }} className="bg-primary bg-opacity-55 text-white" >Price </MenuItem>
                             
                             <MenuItem value={'date'}
-                                className="bg-primary bg-opacity-55 text-white"
+                                className="bg-primary bg-opacity-55 mt-4 text-white"
                                 onClick={() => { setSort('date'), refetch() }}>Date</MenuItem>
                         </MenuList>
                     </Menu>
@@ -122,8 +172,9 @@ const Products = () => {
                         </MenuList>
                     </Menu>
                 </div>
-                <div className="md:flex gap-3 ">
-                    <input
+                <div className="md:flex gap-3 items-center">
+                    <div className="flex flex-col gap-2">
+                        <input
                         type="number"
                         placeholder="Min Price"
                         className="w-12 py-2 pl-4 text-sm rounded-md sm:w-auto focus:outline-none text-black bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-violet-600" 
@@ -137,6 +188,8 @@ const Products = () => {
                         value={maxPrice}
                         onChange={(e) => setMaxPrice(e.target.value)}
                     />
+                    </div>
+                    
                     <Button className="bg-primary text-white" onClick={handleMinMax}>Filter</Button>
                 </div>
 
